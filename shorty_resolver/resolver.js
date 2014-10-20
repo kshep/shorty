d39368189a6ec9cb920b766e15a8d71f9b752ac1
Ken Sheppardson <ken@sofi.com>
2014-10-20 13:59:48 -0700

    Minor whitespace cleanup

--- shorty_resolver/resolver.js
+++ shorty_resolver/resolver.js
@@ -12,7 +12,6 @@ conn.connect()
 http.createServer(function(req,res) {
 
     label = req.url.replace(/\//g,'');
-
     console.log("Querying for %s",label)
 
     conn.query('SELECT long_url from shorturl WHERE label = ?',[label],function(err,rows){
@@ -23,9 +22,7 @@ http.createServer(function(req,res) {
       if (rows.length > 0) {
 
         var long_url = rows[0].long_url;
-
         console.log("Redirect: %s -> %s", label, long_url);
-
         res.writeHead(302, {
             'Location': long_url
         })
@@ -34,9 +31,9 @@ http.createServer(function(req,res) {
       } else {
 
         console.log("Not Found: %s", label);
-
         res.writeHead(404, {"Content-Type": "text/plain"});
         res.end("404 Not Found");
+
       }
 
     })
