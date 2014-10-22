7b4f6dbe1c8dfdd82ded5b7d3288cfe2909019e0
Ken Sheppardson <ken@sofi.com>
2014-10-22 13:38:49 -0700

    Refactor to properly handle async memcached and MySQL calls

--- shorty_resolver/resolver.js
+++ shorty_resolver/resolver.js
@@ -5,7 +5,7 @@ var conn  = mysql.createConnection({
     user     : 'shorty',
     password : 'shorty',
     database : 'shorty'
-})
+});
 
 conn.connect();
 
@@ -16,40 +16,48 @@ http.createServer(function(req,res) {
 
     label = req.url.replace(/\//g,'');
 
-    // Check Cache
     cache.get(label, function(err,long_url) {
-      console.log("Cache: %s -> %s", label, long_url);
-    });
 
-    // Check DB if not cached
-    if ( typeof long_url === 'undefined' ) {
-      conn.query('SELECT long_url from shorturl WHERE label = ?',[label], function(err,rows){
-        if (err)
-          console.log("Error: %s", err);
+      if (err) {
+        console.log("Cache Error: %s", err);
+        res.end("500 Internal Server Error");
 
-        if (rows.length > 0) {
-          long_url = rows[0].long_url;
-          cache.add(label,long_url,1000, function(err) {
+      } else if (long_url) {
+
+        console.log("Cache Redirect: %s -> %s", label, long_url);
+        res.writeHead(302, { "Location": long_url });
+        res.end();
+
+      } else {
+
+        conn.query('SELECT long_url from shorturl WHERE label = ?',[label], function(err,rows){
+
+          if (err) {
             console.log("Error: %s", err);
-          });
-          console.log("DB: %s -> %s", label, long_url);
-        }
-      })
-    }
+            res.end("500 Internal Server Error");
 
-    // Redirect or 404
-    if ( typeof long_url === 'undefined' ) {
-      console.log("Not Found: %s", label);
-      res.writeHead(404, {"Content-Type": "text/plain"});
-      res.end("404 Not Found");
-    } else {
-      console.log("Redirect: %s -> %s", label, long_url);
-      res.writeHead(302, {
-        'Location': long_url
-      })
-      res.end();
-    }
+          } else if (rows.length > 0) {
 
+            long_url = rows[0].long_url;
+            cache.add(label,long_url,1000, function(err) {
+              if (err) {
+                console.log("Cache add error: %s", err);
+              }
+            });
+            console.log("DB Redirect: %s -> %s", label, long_url);
+            res.writeHead(302, { "Location": long_url });
+            res.end();
+
+          } else {
+
+            console.log("Not Found: %s", label);
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.end("404 Not Found");
+
+          }
+        });
+      }
+    });
 }).listen(7654,'127.0.0.1');
 
 console.log('Shorty is listening on 127.0.0.1:7654');
